b60b16345d57aa3bf67fb09e97aa68945ede2114
Kalindu De Costa <kalindu.decosta@mail.utoronto.ca>
2018-08-29 01:51:07 -0400

    knight character can spawn on click
    knight's also wander around randomly

---
+++ Assets/Scripts/env/DayNightCycle.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DayNightCycle : MonoBehaviour {
+
+    public float dayDuration = 8.0f;
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate ( Vector3.up * ( dayDuration * Time.deltaTime ) );
+	}
+}
