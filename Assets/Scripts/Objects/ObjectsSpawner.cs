2c7712d8db2ba6fc9044d3560231ebbb90122cff
sakshaat_dc <sakshaat_dc@hotmail.com>
2018-09-05 18:34:53 -0400

    started work on walls, rotation works on spawner and grid

--- Assets/Scripts/Objects/ObjectsSpawner.cs
+++ Assets/Scripts/Objects/ObjectsSpawner.cs
@@ -5,22 +5,37 @@ using UnityEngine;
 public class ObjectsSpawner : MonoBehaviour {
 
     public Camera camera;
-    public GameObject prefab;
+    public int i;
+    public int rot;
+    public GameObject[] objectArray;
+    private const string GRID_NAME = "grid";
+
+    void rotate() {
+        rot = (rot + 90) % 360;
+    }
+    
+
+
+    void instantiateObj() {
+        RaycastHit hit;
+        Ray ray = camera.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(ray, out hit)) {
+            if(hit.transform.gameObject.name == GRID_NAME) {
+                // spawn the object
+                Instantiate(objectArray[i], hit.point, Quaternion.Euler(new Vector3(0, rot, 0)));
+            }
+        }
+    }
 
 	// Update is called once per frame
 	void Update () {
-		 
-        // check for mouse clicks
-        if (Input.GetMouseButtonUp(0)) {
+        if(Input.GetKeyUp(KeyCode.Alpha1)) i = 0;
+        if(Input.GetKeyUp(KeyCode.Alpha2)) i = 1;
+        if(Input.GetKeyUp(KeyCode.R)) rotate();
 
-            Debug.Log("Left mouse clicked");
-            RaycastHit hit;
-            Ray ray = camera.ScreenPointToRay(Input.mousePosition);
-
-            if (Physics.Raycast(ray, out hit)) {
-                Debug.Log("spawwning");
-                Instantiate(prefab, hit.point, Quaternion.identity);
-            }
+        if(Input.GetMouseButtonUp(0)) {
+            instantiateObj();
         }
 	}
 }
