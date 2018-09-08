45f5ff92f168e2be8d5f61c76bfb0eec38337a99
sakshaat_dc <sakshaat_dc@hotmail.com>
2018-09-08 13:05:43 -0400

    added a simple gui to switch between what is being spawned

--- Assets/Scripts/Objects/ObjectsSpawner.cs
+++ Assets/Scripts/Objects/ObjectsSpawner.cs
@@ -5,37 +5,52 @@ using UnityEngine;
 public class ObjectsSpawner : MonoBehaviour {
 
     public Camera camera;
+    public GameObject[] objectArray;
+
     public int i;
     public int rot;
-    public GameObject[] objectArray;
+
+    private const string SPAWNABLE_TAG = "Spawnable";
     private const string GRID_NAME = "grid";
 
-    void rotate() {
+    public bool isDeleting;
+
+    public void rotate() {
         rot = (rot + 90) % 360;
     }
-    
 
+    public void toggleDeleting() {
+        isDeleting = !isDeleting;
+    }
 
-    void instantiateObj() {
+    void mouseClicked() {
         RaycastHit hit;
         Ray ray = camera.ScreenPointToRay(Input.mousePosition);
 
         if (Physics.Raycast(ray, out hit)) {
-            if(hit.transform.gameObject.name == GRID_NAME) {
-                // spawn the object
+            print(hit.transform.gameObject.tag);
+            // spawn when not deleting
+            if(!isDeleting && hit.transform.gameObject.name == GRID_NAME) {
                 Instantiate(objectArray[i], hit.point, Quaternion.Euler(new Vector3(0, rot, 0)));
+            } else if(isDeleting && hit.transform.gameObject.tag == SPAWNABLE_TAG) {
+                Destroy(hit.transform.gameObject);
             }
         }
     }
 
+    public void setI(int j) {
+        isDeleting = false;
+        i = j;
+    } 
+
 	// Update is called once per frame
 	void Update () {
-        if(Input.GetKeyUp(KeyCode.Alpha1)) i = 0;
-        if(Input.GetKeyUp(KeyCode.Alpha2)) i = 1;
+        if(Input.GetKeyUp(KeyCode.Alpha1)) setI(0);
+        if(Input.GetKeyUp(KeyCode.Alpha2)) setI(1);
         if(Input.GetKeyUp(KeyCode.R)) rotate();
 
         if(Input.GetMouseButtonUp(0)) {
-            instantiateObj();
+            mouseClicked();
         }
 	}
 }
