f46073a07be27e4f8e04612962ddbf84faa0fb39
Kalindu De Costa <kalindu.decosta@mail.utoronto.ca>
2018-09-09 15:28:48 -0400

    Object helth script is ready

---
+++ Assets/Scripts/Objects/Life.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Life : MonoBehaviour {
+
+    public float health = 100.0f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+        if (health <= 0) {
+            DestroySelf();
+        }
+	}
+
+    void AddDamage(float damage) {
+
+        Debug.Log("taking damage");
+        health -= damage;
+    }
+
+    void DestroySelf() {
+        Debug.Log("destroyyy");
+        Destroy(this.gameObject);
+    }
+}
